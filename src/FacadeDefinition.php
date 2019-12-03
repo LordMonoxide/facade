875790971aea871004be9deee2f95da88223ae42
Corey Frenette <lordmonoxide@gmail.com>
2019-12-03 08:01:10 -0400

    [#4] Rework tailored facades

--- src/FacadeDefinition.php
+++ src/FacadeDefinition.php
@@ -2,8 +2,11 @@
 
 use BapCat\Phi\Ioc;
 use BapCat\Propifier\PropifierTrait;
+use BapCat\Values\Regex;
+use BapCat\Values\Text;
 
 use ReflectionClass;
+use ReflectionException;
 
 /**
  * @property-read  string  $name
@@ -56,13 +59,77 @@ class FacadeDefinition {
 
   /**
    * @return  array
+   *
+   * @throws  ReflectionException
    */
   public function toArray(): array {
+    $reflect = new ReflectionClass($this->binding);
+    $methods = [];
+    $imports = [];
+
+    $throwsRegex = new Regex('/@throws\s+([^\\ ]+)\s/');
+    $useStatementParser = new UseStatementParser();
+
+    foreach($reflect->getMethods() as $method) {
+      if($method->isPublic() && !($method->isConstructor() || $method->isDestructor() || strpos($method->getName(), '__') === 0)) {
+        $methods[] = $method;
+
+        foreach($method->getParameters() as $param) {
+          if($param->getType() !== null && !$param->getType()->isBuiltin()) {
+            $imports[] = $param->getType()->getName();
+          }
+        }
+
+        if($method->hasReturnType() && !$method->getReturnType()->isBuiltin()) {
+          $imports[] = $method->getReturnType()->getName();
+        }
+
+        if($method->getDocComment() !== false) {
+          foreach($throwsRegex->capture(new Text($method->getDocComment())) as $exception) {
+            $exception = trim((string)reset($exception));
+
+            $methodImports = $useStatementParser->parseUseStatements($method->getDeclaringClass());
+            $found = false;
+
+            foreach($methodImports as $methodImport) {
+              if(substr_compare($methodImport, $exception, -strlen($exception)) === 0) {
+                $found = true;
+                $imports[] = $methodImport;
+                break;
+              }
+            }
+
+            if(!$found) {
+              $imports[] = $method->getDeclaringClass()->getNamespaceName() . '\\' . $exception;
+            }
+          }
+        }
+      }
+    }
+
+    $imports = array_unique($imports);
+    $imports = array_filter($imports, function($import) {
+      // Don't add import if the import has the same class name as the facade (TODO: this is not ideal...)
+      if(substr_compare($import, $this->name, -strlen($this->name)) === 0) {
+        return false;
+      }
+
+      // Don't add imports for classes in the global namespace since facades are generated in the global namespace
+      if(strpos($import, '\\') === false) {
+        return false;
+      }
+
+      // Only add imports that actually exist
+      return class_exists($import) || interface_exists($import);
+    });
+
     return [
       'name'    => $this->name,
       'binding' => $this->binding,
       'ioc'     => get_class($this->ioc),
-      'reflect' => new ReflectionClass($this->ioc->resolve($this->binding)),
+      'reflect' => new ReflectionClass($this->binding),
+      'methods' => $methods,
+      'imports' => $imports,
     ];
   }
 }
